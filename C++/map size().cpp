1da4bf25921fae9646e187c89f0b83b7c72f9032
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2021-10-03 16:22:47 +0530

    map_STL_cpp

---
+++ C++/map size().cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main ()
+{
+    map<int,string> m{ {1,"nikhilesh"} , {2,"shrikant"} , {3,"ashish"} };
+    
+    
+    cout << m.size();
+}
